b79dc6c30efa2fda0680027f890e6c8dababe41d
eom <eom@CP4480.interno.matera.com>
2019-10-26 02:09:32 -0300

    workshodata

---
+++ src/Components/Workshop/WorkshopData.ts
@@ -0,0 +1,53 @@
+export const WorkshopData = [
+
+    {
+        title: "JWA - Java Web Aplication",
+        imgPath:
+            'https://lh6.googleusercontent.com/DIRFeYedIIWMLlE4cpdy5ZbiOuMonqgxwwV6wXyn-dDMB6dqbG7xV5-ra7OcRgkBflIBgQowLsPI8WAYwVKrn9DJwYR-WgjaDiqwmvF9wYekiDO_yXs=w472',
+        name:"Victoria Deorio",
+        company:"Software Developer no CPqD",
+        resume:"Desenvolvedora Backend, atua no ramo da tecnologia há menos de 1 ano. Tem sede de conhecimento e busca sempre maneiras de produzir mais, aprender mais e compartilhar mais.\n" +
+            "Atualmente desenvolve em Java, e em Agosto, foi certificada como Java SE Programmer I pela Oracle. Ainda este ano, ela e sua equipe obtiveram o 3º lugar mundial no Health Hackathon 2019, com um projeto de Inteligência Artificial para leitura de exames por imagem.",
+        "about":"Nesse Hands On você irá construir uma aplicação Server side em Java com Spring Boot Framework. Usando o padrão MVC iremos expor serviços REST que serão consumidos por uma aplicação client JS. Veremos também como implementar testes unitários com JUnit."
+    },
+    {
+        title: "Componentes React",
+        imgPath:
+            'https://lh6.googleusercontent.com/gelOKlMvEadBZZoxkEtVyZ3BXSsXPuLXVd9bnIDIaVhNmotjZPALbw4AS_TK4rEzzzpu0vO-NazGUTApsnXh95VXLF_YkbpPjBgdb4P1kPPWhHWa_Q=w472',
+        name:"Eduardo de Oliveira Moreira",
+        company:"Desenvolvedor na MATERA \n",
+        resume:"Atualmente desenvolvedor Front-End de aplicativos hibridos React/React Native e aplicativos Angular 4 + Ionic3.\n" +
+            "Experiencia com Back-End utilizando Spring MVC + Spring Rest, JPA Hibernate, Java 8 (utilizando Lambda e API Streams), desenvolvimento de testes com Appium, Selenium WebDriver, Arquillian Graphene, Arquillian Drone e Jest.\n" +
+            "Conhecimento em prototipagem com Arduino / Raspberry PI e Impressão 3D.",
+        "about":"Os componentes são os elementos básicos de qualquer aplicativo React. E um aplicativo React típico terá muitos deles, vamos entender um pouco sobre o que é React? o que são os componentes? Por que desenvolver componentes? Nesse Hands On iremos entender um pouco sobre essa biblioteca javascript criada para facilicar a criaão de interfaces de usuario (UI)."
+    },
+    {
+        title: "Engenharia de Features utilizando Pandas",
+        imgPath:
+            'https://lh5.googleusercontent.com/ZrVa_3-mli8pyWTRNimGoFLL7B5RQ6hMIT8E8mpPHkuHuEURdSSMZuOBjoI3S-t9hMRkMZUIeuH3IAn2QJslgZN7Y5BrPWxo3V2BOcxPD-WNCSm_OGg=w472',
+        name:"Luiz Amaral",
+        company:"Machine Learning Researcher at TARVOS",
+        resume:"Formado em Física pela Universidade Federal do Paraná, atua como desenvolvedor Python há 6 anos, trabalhando em projetos de hardware embarcado, engenharia de dados e aprendizado de máquina. Conhece também um pouco de web, games e tem um pezinho em open source.",
+        "about":"Será que Jack tinha chances de sobreviver ao naufrágio do Titanic?\n" +
+            "Neste workshop vamos estudar o Pandas, uma biblioteca de análise de dados para Python.\n" +
+            "Com o Pandas estudaremos o dataset dos passageiros do Titanic em uma aventura menos romântica que o filme.\n" +
+            "Veremos como indexar, agrupar, visualizar e interpretar os dados e, por último, qual é o impacto da engenharia de features em modelos de aprendizado de máquina."
+    },
+    {
+        title: "UX Designer",
+        imgPath:
+            'https://lh5.googleusercontent.com/yCKkxKWp8qqY6pGCf0Klnh7wUq3kUy05XH0OfaCfaAJgad4NH9tcfVisruw6oNdC188IFAukJ-kNWmjLCy-s7b_iwEWw3-B4DgEFZbX7UC3GJ4E89ZQ=w472',
+        name:"César Borges",
+        company:"UX Researcher na Dextra Digita",
+        resume:"Especialista em Tecnologias e Inovações para Web, Graduado em Tecnologia em Análise e Desenvolvimento de Sistemas / Sistemas da Informação, também Graduado em Matemática. Colaborador da Dextra há mais de 06 anos, atuando principalmente com UX (User eXperience).",
+        "about":"UX Designer (Dinâmica de usabilidade, apresentação de testes e usabilidade)."
+    },
+    {
+        title: "Front-end / Back-end  JS",
+        imgPath: 'https://lh3.googleusercontent.com/KoCI0GqPIcidCDLRXQrZzgMVgtTvUwALgT-iGwWb53iVXkarGSHdm90IhF9_Y6sFNWegR3rL73J_FhafTcEIFcfFB8R62XxaDAGLLu8KgVU5YgH9Cg=w472',
+        name:"Márcio Souza ",
+        company:"Developer at Dextra Digital",
+        resume:"Adminsitrative Management at ESAMC - Escola Superior de Administração, Marketing e Comunicação, Técnico / Tecnólogo em Informática Unicamp / Cotuca. Atua há mais de dez anos na área de Tecnologia em Informação.",
+        "about":"Neste hands desenvolveremos uma aplicação em Javascript Front-end / Back-end."
+    },
+];
